a30d8665511c72eec343f6eff8e8d9b0f09774a6
toonybg <antoni.markovski@gmail.com>
2020-01-13 21:22:33 +0100

    add class Employee

--- code/first.py
+++ code/first.py
@@ -1,1 +1,21 @@
-print("first python document")
+print("-------------------")
+
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self,name,lastname,pay):
+        self.name = name
+        self.lastname = lastname
+        self.email = name + lastname + "@company.com"
+        self.pay = pay
+
+emp_1 = Employee("Toony","bg",48000)
+
+print(emp_1.email)
+
+
+
+
+
+
+print("-------------------")
